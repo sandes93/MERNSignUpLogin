4f97e0893841dfc27e2dd89d8d5f36f1d9e03127
Sandes <sandeshpathak0112@gmail.com>
2020-08-02 21:08:24 +1000

    removed serverurl

--- client/src/components/UserFunctions/userFunctions.js
+++ client/src/components/UserFunctions/userFunctions.js
@@ -2,15 +2,12 @@ import axios from 'axios';
 
 //change port
 let serverUrl = "http://localhost:5001"
-// (change serverUrl on heroku)
-// if (process.env.NODE_ENV === "production") {
-//     serverUrl = 'https://{heroku}.com/';
-// 
+let herokuURL= window.location.origin;
 
 export const registerUser = userData => {
     // console.log(userData);
     return axios
-    .post(serverUrl + '/api/register', {
+    .post('/api/register', {
         first_name: userData.first_name,
         last_name: userData.last_name,
         email: userData.email,
@@ -24,7 +21,7 @@ export const registerUser = userData => {
 export const loginUser = userData => {
     // console.log(userData);
     return axios
-    .post(serverUrl + '/api/login', {
+    .post('/api/login', {
         email: userData.email,
         password: userData.password
     })
@@ -39,7 +36,7 @@ export const loginUser = userData => {
 
 export const getUsers = userData => {
     return axios
-    .get(serverUrl + '/api/displayusers', {
+    .get('/api/displayusers', {
     })
     .then(response => {
         // console.log(response.data);
