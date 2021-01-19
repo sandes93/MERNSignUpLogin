932d96d181c4c0fda47b65d2770292785263e9ff
ali <alinaeem2006@gmail.com>
2021-01-19 19:17:57 +1100

    resolve conflict

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(
 app.use(express.static("public"));
 // DB Config
 const MONGODB_URI = ""
-const mongoURI = "mongodb://localhost/mernlogin";
+const mongoURI = "mongodb+srv://trilogy:trilogy@cluster0.u8m9s.mongodb.net/mernlogin?retryWrites=true&w=majority";
 
 // Connect to MongoDB
 mongoose.connect( MONGODB_URI || mongoURI, {
