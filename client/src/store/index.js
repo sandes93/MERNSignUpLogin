d48ae9ecf2263fbc923fd3ed152b96bd99574366
Jack McNicol <jackmcpickle@gmail.com>
2021-02-09 20:12:07 +1030

    initial context should check token is valid for user

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,6 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import React, { createContext, useContext, useEffect, useReducer } from 'react';
+import { SET_CURRENT_USER } from '../actions/types';
+import { getUser } from '../utils/userApis';
 import { appReducer, appInitialState } from './appStore';
 
 const AppContext = createContext([
@@ -10,6 +12,12 @@ const { Provider } = AppContext;
 
 export const AppContextProvider = ({ children, ...props }) => {
     const [appState, appDispatch] = useReducer(appReducer, appInitialState);
+
+    useEffect(() => {
+        console.log('Initial User check. Verify token saved');
+        getUser().then(res => appDispatch({ type: SET_CURRENT_USER, payload: res.data }))
+            .catch(() => appDispatch({ type: SET_CURRENT_USER, payload: {}}))
+    }, [])
 
     return (
         <Provider
